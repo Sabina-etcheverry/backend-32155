2b0d8fac4e2dd6e472e5c303297a074072c9f2d5
Sabina Etcheverry <sabinaetcheverry99@gmail.com>
2022-09-28 19:16:34 -0300

    Se mejora el código

--- Clase_1/index.js
+++ Clase_1/index.js
@@ -18,33 +18,30 @@ class Usuario {
         return console.log(`Tiene ${this.mascotas.length} mascotas`)
     }
 
-    addBook() {
-        this.libros.push({nombre: "Encuentros", autor: "Gabriel Rolon"})
+    addBook(libro) {
+        this.libros.push(libro)
     }
 
     getBookNames() {
-        let arrayLibros = []
-        this.libros.forEach((e) => arrayLibros.push(e.nombre, e.autor));
-        return arrayLibros;
+        const arrayLibros= this.libros.map(arrayNuevo => arrayNuevo.name)
     }
 
-    showBook(name) {
-        name.forEach((nombre) => console.log(`El nombre del libro es: ${nombre}`));
-    }
-
-    showAuthorBook (author) {
-        author.forEach((autor) => console.log(`Del autor ${autor}`))
+    showBook() {
+      this.libros.forEach((unLibro) => {
+        console.log(`El nombre del libro es: "${unLibro.name}" y el autor es "${unLibro.autor}"`);
+      })
     }
 }
 
 const usuario1 = new Usuario("Sabina", "Etcheverry");
 
+usuario1.addBook({ name: "Encuentros", autor: "Gabriel Rolon"})
+
 usuario1.getFullName();
-usuario1.addMascota("perro", "gato");
+usuario1.addMascota("perro");
+usuario1.addMascota("gato");
 usuario1.countMascotas();
-usuario1.addBook();
 let arrayLibros = usuario1.getBookNames()
-usuario1.showBook(arrayLibros);
-usuario1.showAuthorBook(arrayLibros);
+usuario1.showBook();
 
 
