7bfdfd3a7dc7819c35fbe3d9a246fa6b3b7b8ec4
Sabina Etcheverry <sabinaetcheverry99@gmail.com>
2022-10-24 12:44:57 -0300

    Se modifica server.js

--- src/services/server.js
+++ src/services/server.js
@@ -3,6 +3,7 @@ const mainRouter = require('../routes/index');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
